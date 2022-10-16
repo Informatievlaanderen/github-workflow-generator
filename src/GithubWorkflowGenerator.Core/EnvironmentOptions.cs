8a8664b0ad618a7adfba2cb615de6b2f5d243b81
Luk Vermeulen <luk.vermeulen@gmail.com>
2022-10-16 21:00:02 +0200

    fis: add releaselib command

--- src/GithubWorkflowGenerator.Core/EnvironmentOptions.cs
+++
@@ -1,5 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace GithubWorkflowGenerator.Core;
-
-public record EnvironmentOptions(string PublishFolderForLambda, string S3BucketForLambda, IEnumerable<string> ServiceMatrix);
