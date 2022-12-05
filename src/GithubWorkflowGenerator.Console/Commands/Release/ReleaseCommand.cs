5166d1ad985f949e72da173ce89ccf8640b4d3d7
Luk Vermeulen <luk.vermeulen@gmail.com>
2022-12-05 16:12:21 +0100

    fix: add workflowName & skipLambda & persist-credentials false

--- src/GithubWorkflowGenerator.Console/Commands/Release/ReleaseCommand.cs
+++ src/GithubWorkflowGenerator.Console/Commands/Release/ReleaseCommand.cs
@@ -17,11 +17,13 @@ internal class ReleaseCommand : Command
     public ReleaseCommand()
         : this(CommandName, CommandDescription)
     {
-        var releaseFileName = new Option<string>(new[] { "--fileName" }, () => "release.yml", "Output file name.");
+        var releaseFileName = new Option<string>(new[] { "--fileName" }, () => "release.yml", "Output file name");
+        var workflowName = new Option<string>(new[] { "--workflowName" }, () => "Release", "Workflow name");
         var repositoryName = new Option<string>(new[] { "--repositoryName" }, "Repository name (e.g. streetname-registry)");
         var repositoryPrefix = new Option<string>(new[] { "--repositoryPrefix" }, "Repository prefix (e.g. sr for streetname-registry)");
         var buildArtifacts = new Option<List<string>>(new[] { "--buildArtifacts" }, "Build artifacts (space separated)") { AllowMultipleArgumentsPerToken = true };
         var nugetPackages = new Option<List<string>>(new[] { "--nugetPackages" }, "Nuget packages (space separated list of Nuget package names)") { AllowMultipleArgumentsPerToken = true };
+        var skipLambda = new Option<bool>(new[] { "--skipLambda" }, () => false, "Skips generation of lambda-related actions");
         var lambdaSourceFolder = new Option<string>(new[] { "--lambdaSourceFolder" }, "Folder where the lambda function is zipped from.");
         var testS3BucketForLambda = new Option<string>(new[] { "--testS3BucketForLambda" }, "Test S3 bucket for lambda (in format s3://some.bucket.name)");
         var testServiceMatrix = new Option<List<string>>(new[] { "--testServiceMatrix" }, "List of services to deploy on Test (space separated)") { AllowMultipleArgumentsPerToken = true };
@@ -31,10 +33,12 @@ internal class ReleaseCommand : Command
         var productionServiceMatrix = new Option<List<string>>(new[] { "--productionServiceMatrix" }, "List of services to deploy on Production (space separated)") { AllowMultipleArgumentsPerToken = true };
 
         AddOption(releaseFileName);
+        AddOption(workflowName);
         AddOption(repositoryName);
         AddOption(repositoryPrefix);
         AddOption(buildArtifacts);
         AddOption(nugetPackages);
+        AddOption(skipLambda);
         AddOption(lambdaSourceFolder);
         AddOption(testS3BucketForLambda);
         AddOption(testServiceMatrix);
@@ -42,11 +46,15 @@ internal class ReleaseCommand : Command
         AddOption(stagingServiceMatrix);
         AddOption(productionS3BucketForLambda);
         AddOption(productionServiceMatrix);
-        this.SetHandler(async context => await Handle(context!.FileName, context.RepositoryName, context.RepositoryPrefix, context.BuildArtifacts, context.NuGetPackages, context.LambdaSourceFolder,
+        this.SetHandler(async context => await Handle(context!.FileName, context.WorkflowName, context.RepositoryName, context.RepositoryPrefix, context.BuildArtifacts, context.NuGetPackages,
+                context.SkipLambda,
+                context.LambdaSourceFolder,
                 context.TestS3BucketForLambda, context.TestServiceMatrix,
                 context.StagingS3BucketForLambda, context.StagingServiceMatrix,
                 context.ProductionS3BucketForLambda, context.ProductionServiceMatrix),
-            new ReleaseCommandInputBinder(releaseFileName, repositoryName, repositoryPrefix, buildArtifacts, nugetPackages, lambdaSourceFolder,
+            new ReleaseCommandInputBinder(releaseFileName, workflowName, repositoryName, repositoryPrefix, buildArtifacts, nugetPackages,
+                skipLambda,
+                lambdaSourceFolder,
                 testS3BucketForLambda, testServiceMatrix,
                 stagingS3BucketForLambda, stagingServiceMatrix,
                 productionS3BucketForLambda, productionServiceMatrix));
@@ -56,13 +64,17 @@ internal class ReleaseCommand : Command
         : base(name, description)
     { }
         
-    private static async Task Handle(string fileName, string repositoryName, string repositoryPrefix, IEnumerable<string> buildArtifacts, Dictionary<string, string> nugetPackages, string lambdaSourceFolder,
+    private static async Task Handle(string fileName, string workflowName, string repositoryName, string repositoryPrefix, IEnumerable<string> buildArtifacts, Dictionary<string, string> nugetPackages,
+        bool skipLambda,
+        string lambdaSourceFolder,
         string testS3BucketForLambda, IEnumerable<string> testServiceMatrix,
         string stagingS3BucketForLambda, IEnumerable<string> stagingServiceMatrix,
         string productionS3BucketForLambda, IEnumerable<string> productionServiceMatrix)
     {
         var generator = new GithubGenerator();
-        var options = new ReleaseGeneratorOptions(repositoryName, repositoryPrefix, buildArtifacts, nugetPackages.Select(x => new NuGetArtifactAndPackage(x.Key, x.Value)), lambdaSourceFolder,
+        var options = new ReleaseGeneratorOptions(workflowName, repositoryName, repositoryPrefix, buildArtifacts, nugetPackages.Select(x => new NuGetArtifactAndPackage(x.Key, x.Value)),
+            skipLambda,
+            lambdaSourceFolder,
             new EnvironmentOptions(testS3BucketForLambda, testServiceMatrix),
             new EnvironmentOptions(stagingS3BucketForLambda, stagingServiceMatrix),
             new EnvironmentOptions(productionS3BucketForLambda, productionServiceMatrix));
