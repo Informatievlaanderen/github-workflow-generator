830de25b07b73f269757c7b8aa05d31b254fdad0
Luk Vermeulen <luk.vermeulen@gmail.com>
2022-12-16 10:07:45 +0100

    fix: add workflowName to releaselib

--- src/GithubWorkflowGenerator.Console/Commands/ReleaseLib/ReleaseLibCommand.cs
+++ src/GithubWorkflowGenerator.Console/Commands/ReleaseLib/ReleaseLibCommand.cs
@@ -17,26 +17,28 @@ internal class ReleaseLibCommand : Command
         : this(CommandName, CommandDescription)
     {
         var releaseFileName = new Option<string>(new[] { "--fileName" }, () => "release.yml", "Output file name.");
+        var workflowName = new Option<string>(new[] { "--workflowName" }, () => "Release", "Workflow name");
         var nugetPackages = new Option<List<string>>(new[] { "--nugetPackages" }, "Nuget packages (space separated)") { AllowMultipleArgumentsPerToken = true };
         var jiraPrefix = new Option<string>(new[] { "--jiraPrefix" }, "Prefix for JIRA project");
         var jiraProject = new Option<string>(new[] { "--jiraProject" }, () => "GAWR", "JIRA project");
 
         AddOption(releaseFileName);
+        AddOption(workflowName);
         AddOption(nugetPackages);
         AddOption(jiraPrefix);
         AddOption(jiraProject);
-        this.SetHandler(async context => await Handle(context!.FileName, context.NuGetPackages, context.JiraPrefix, context.JiraProject),
-            new ReleaseLibCommandInputBinder(releaseFileName, nugetPackages, jiraPrefix, jiraProject));
+        this.SetHandler(async context => await Handle(context!.FileName, context.WorkflowName, context.NuGetPackages, context.JiraPrefix, context.JiraProject),
+            new ReleaseLibCommandInputBinder(releaseFileName, workflowName, nugetPackages, jiraPrefix, jiraProject));
     }
 
     public ReleaseLibCommand(string name, string? description = null)
         : base(name, description)
     { }
         
-    private static async Task Handle(string fileName, IEnumerable<string> nugetPackages, string jiraPrefix, string jiraProject)
+    private static async Task Handle(string fileName, string workflowName, IEnumerable<string> nugetPackages, string jiraPrefix, string jiraProject)
     {
         var generator = new GithubGenerator();
-        var options = new ReleaseLibGeneratorOptions(nugetPackages, jiraPrefix, jiraProject);
+        var options = new ReleaseLibGeneratorOptions(workflowName, nugetPackages, jiraPrefix, jiraProject);
         string result = await generator.GenerateReleaseLibWorkflowAsync(options);
         await File.WriteAllTextAsync(fileName, result);
 
--- src/GithubWorkflowGenerator.Console/Commands/ReleaseLib/ReleaseLibCommandInput.cs
+++ src/GithubWorkflowGenerator.Console/Commands/ReleaseLib/ReleaseLibCommandInput.cs
@@ -4,18 +4,20 @@ using System.CommandLine.Binding;
 
 namespace GithubWorkflowGenerator.Console.Commands.ReleaseLib;
 
-public record ReleaseLibCommandInput(string FileName, List<string> NuGetPackages, string JiraPrefix, string JiraProject);
+public record ReleaseLibCommandInput(string FileName, string WorkflowName, List<string> NuGetPackages, string JiraPrefix, string JiraProject);
 
 public class ReleaseLibCommandInputBinder : BinderBase<ReleaseLibCommandInput?>
 {
     private readonly Option<string> _fileName;
+    private readonly Option<string> _workflowName;
     private readonly Option<List<string>> _nugetPackages;
     private readonly Option<string> _jiraPrefix;
     private readonly Option<string> _jiraProject;
 
-    public ReleaseLibCommandInputBinder(Option<string> fileName, Option<List<string>> nugetPackages, Option<string> jiraPrefix, Option<string> jiraProject)
+    public ReleaseLibCommandInputBinder(Option<string> fileName, Option<string> workflowName, Option<List<string>> nugetPackages, Option<string> jiraPrefix, Option<string> jiraProject)
     {
         _fileName = fileName;
+        _workflowName = workflowName;
         _nugetPackages = nugetPackages;
         _jiraPrefix = jiraPrefix;
         _jiraProject = jiraProject;
@@ -24,14 +26,15 @@ public class ReleaseLibCommandInputBinder : BinderBase<ReleaseLibCommandInput?>
     protected override ReleaseLibCommandInput? GetBoundValue(BindingContext bindingContext)
     {
         var fileName = bindingContext.ParseResult.GetValueForOption(_fileName);
+        var workflowName = bindingContext.ParseResult.GetValueForOption(_workflowName);
         var nugetPackages = bindingContext.ParseResult.GetValueForOption(_nugetPackages);
         var jiraPrefix = bindingContext.ParseResult.GetValueForOption(_jiraPrefix);
         var jiraProject = bindingContext.ParseResult.GetValueForOption(_jiraProject);
-        if (fileName is null || nugetPackages is null || jiraPrefix is null || jiraProject is null)
+        if (fileName is null || workflowName is null || nugetPackages is null || jiraPrefix is null || jiraProject is null)
         {
             return default;
         }
 
-        return new ReleaseLibCommandInput(fileName, nugetPackages, jiraPrefix, jiraProject);
+        return new ReleaseLibCommandInput(fileName, workflowName, nugetPackages, jiraPrefix, jiraProject);
     }
 }
